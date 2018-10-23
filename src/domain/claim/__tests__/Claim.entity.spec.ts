e707ea54bf488931beee7fc19028a0e82abf6391
Igor Kamyshev <igor@kamyshev.me>
2018-10-23 11:42:24 +0300

    Fix tests

--- src/domain/claim/__tests__/Claim.entity.spec.ts
+++ src/domain/claim/__tests__/Claim.entity.spec.ts
@@ -1,11 +1,21 @@
 import InvariantViolationException from '../../exception/InvariantViolationException'
 import Quota from '../../quota/Quota.entity'
+import User from '../../user/User.entity'
+import Applicant from '../Applicant.vo'
 import Claim from '../Claim.entity'
 
 describe('Claim', () => {
+  let applicant: Applicant
+  let user: User
+
+  beforeAll(() => {
+    applicant = new Applicant('Petr', 12, 'q', 'Tomsk')
+    user = new User('login')
+  })
+
   describe('bindQuota', () => {
     test('should bind quota correctly', () => {
-      const c = new Claim('1', 'Petro')
+      const c = new Claim('1', applicant, user, 'theme')
       const q = new Quota('1', 'quota first')
 
       c.bindQuota(q)
@@ -15,7 +25,7 @@ describe('Claim', () => {
     })
 
     test('should throw exception if quota already binded', () => {
-      const c = new Claim('1', 'Petro')
+      const c = new Claim('1', applicant, user, 'theme')
       const oldQ = new Quota('1', 'quota first')
       const newQ = new Quota('2', 'quota second')
 
@@ -29,7 +39,7 @@ describe('Claim', () => {
 
   describe('unbindQuota', () => {
     test('should unbind quota correctly', () => {
-      const c = new Claim('1', 'Petro')
+      const c = new Claim('1', applicant, user, 'theme')
       const q = new Quota('1', 'quota first')
 
       c.bindQuota(q)
@@ -40,7 +50,7 @@ describe('Claim', () => {
     })
 
     test('should throw excaption ig try to unbind empty quota', () => {
-      const c = new Claim('1', 'Petro')
+      const c = new Claim('1', applicant, user, 'theme')
 
       expect(
         () => c.unbindQuota(),
