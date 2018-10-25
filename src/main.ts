797ff733ecf117612bc29106eb9169b796c7cf93
uncleseneca <uncleseneca@gmail.com>
2018-10-25 20:19:55 +0300

    added options for cors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import * as cors from 'cors'
 import { join } from 'path'
 
 import { AppModule } from '@app/app.module'
@@ -10,6 +11,24 @@ async function bootstrap() {
   setupSwagger(app, 'docs')
 
   app.useStaticAssets(join(__dirname, '..', 'public'))
+
+  const corsOptions = {
+    origin: '*',
+    credentials: true,
+    allowHeaders: [
+      'DNT',
+      'User-Agent',
+      'X-Requested-With',
+      'If-Modified-Since',
+      'Cache-Control',
+      'Content-Type',
+      'Range',
+      'Authorization',
+      'Cookie',
+    ],
+    methods: ['GET', 'POST', 'OPTIONS'],
+  }
+  app.use(cors(corsOptions))
 
   await app.listen(3000)
 }
