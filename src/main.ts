6b3145c57ac9449a9a54c58fec4dcc230620e79b
uncleseneca <uncleseneca@gmail.com>
2018-10-26 14:03:16 +0300

    removed corsOptions from main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,6 @@ import { AppModule } from '@app/app.module'
 import corsMiddleware from '@app/corsMiddleware'
 import setupSwagger from '@app/infrastructure/swagger'
 
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  allowHeaders: [
-    'DNT',
-    'User-Agent',
-    'X-Requested-With',
-    'If-Modified-Since',
-    'Cache-Control',
-    'Content-Type',
-    'Range',
-    'Authorization',
-    'Cookie',
-  ],
-  methods: ['GET', 'POST', 'OPTIONS'],
-}
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
