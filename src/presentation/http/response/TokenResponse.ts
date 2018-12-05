48cef0277cf1dc3e2ebb4a2b1cbcfa6b4f4c1cfc
iwanttobealight <iwanttobealight@yandex.ru>
2018-12-05 14:39:46 +0300

    add annotation

--- src/presentation/http/response/TokenResponse.ts
+++ src/presentation/http/response/TokenResponse.ts
@@ -2,10 +2,11 @@ import { ApiModelProperty } from '@nestjs/swagger'
 
 // tslint:disable-next-line:max-line-length
 const exampleToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1Njg4MTE5ODUsImlkIjoxLCJzZXNzaW9uSWQiOiI1YmEwZjg1MWFiZWExIn0.XCe-rLCBQb1rPKhK8RYiu8WEmN3ZFSFbUuBfG4hgbxo'
-
+const exampleRoles = ['admin', 'client']
 export default class TokenResponse {
   @ApiModelProperty({ example: exampleToken })
   public readonly token: string
+  @ApiModelProperty({ example: exampleRoles })
   public readonly roles: string[]
 
   public constructor(token: string, roles: string[] = []) {
