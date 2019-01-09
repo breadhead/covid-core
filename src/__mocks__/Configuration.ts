4eaacc9fb86b5abefd7ffd2d4690e0bc394413d6
uncleseneca <uncleseneca@gmail.com>
2019-01-09 13:34:30 +0300

    fix(global): fix linter issues

--- src/__mocks__/Configuration.ts
+++ src/__mocks__/Configuration.ts
@@ -3,15 +3,15 @@ import { Option } from 'tsoption'
 import Configuration from '@app/infrastructure/Configuration/Configuration'
 
 export default class MockConfiguration extends Configuration {
-    public constructor(private readonly values: { [key: string]: string }) {
-        super()
-    }
+  public constructor(private readonly values: { [key: string]: string }) {
+    super()
+  }
 
-    public get(key: string): Option<string> {
-        return Option.of(this.values[key])
-    }
+  public get(key: string): Option<string> {
+    return Option.of(this.values[key])
+  }
 
-    public isDev() {
-        return false
-    }
+  public isDev() {
+    return false
+  }
 }
