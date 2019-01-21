3f623d8e8fe46850b952bdb6f69c63630441a8c8
Igor Kamyshev <igor@kamyshev.me>
2019-01-21 15:09:53 +0300

    feat(global): add trusted fields to current user response

--- src/presentation/http/response/CurrentUserResponse.ts
+++ src/presentation/http/response/CurrentUserResponse.ts
@@ -3,9 +3,14 @@ import { ApiModelProperty } from '@nestjs/swagger'
 
 export default class CurrentUserResponse {
   public static fromUser(user: User) {
-    return { roles: user.roles }
+    const trusted = user.isClient ? user.valid : true
+
+    return { roles: user.roles, trusted }
   }
 
   @ApiModelProperty({ example: ['admin', 'case'] })
   public readonly roles: string[]
+
+  @ApiModelProperty({ example: true })
+  public readonly trusted: boolean
 }
