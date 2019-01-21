1b5c5d1370597e96bee06f4ba22b99a4e7c57a1e
Igor Kamyshev <igor@kamyshev.me>
2019-01-21 15:49:03 +0300

    revert(global): remove extra field from current user

--- src/presentation/http/response/CurrentUserResponse.ts
+++ src/presentation/http/response/CurrentUserResponse.ts
@@ -3,14 +3,9 @@ import { ApiModelProperty } from '@nestjs/swagger'
 
 export default class CurrentUserResponse {
   public static fromUser(user: User) {
-    const trusted = user.isClient ? user.valid : true
-
-    return { roles: user.roles, trusted }
+    return { roles: user.roles }
   }
 
   @ApiModelProperty({ example: ['admin', 'case'] })
   public readonly roles: string[]
-
-  @ApiModelProperty({ example: true })
-  public readonly trusted: boolean
 }
