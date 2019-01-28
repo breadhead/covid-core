e3971bbcea8f068d3d9957dc10d4f459cf78ab39
Igor Kamyshev <igor@kamyshev.me>
2019-01-28 15:05:00 +0300

    feat: enable sms sending

--- src/infrastructure/SmsSender/RedSmsSender.ts
+++ src/infrastructure/SmsSender/RedSmsSender.ts
@@ -22,7 +22,7 @@ export default class RedSmsSender implements SmsSender {
 
   public async send(to: string, text: string): Promise<void> {
     if (this.config.isProd()) {
-      await this.redSmsClient.sendSms('REDSMS.RU', '79818907314', '')
+      await this.redSmsClient.sendSms('REDSMS.RU', to, text)
       this.logger.log(`SMS sent to "${to}"`)
     }
 
