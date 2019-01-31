49e395332a14aa764c39ed8b21848e6e9a77c8bc
Vladislav Kobyakov <ayakudere@gmail.com>
2019-01-31 09:59:25 +0300

    fix: common quotas are now not filtered

--- src/presentation/http/response/ClaimQuotaResponse.ts
+++ src/presentation/http/response/ClaimQuotaResponse.ts
@@ -10,27 +10,34 @@ export default class ClaimQuotaResponse {
       }
     }
 
-    const { company } = quota
+    const { company, name, comment } = quota
 
     if (!company) {
-      return {}
+      return {
+        name,
+        comment,
+      }
     }
 
-    const { name, logo, site, comment } = company
+    const { logo, site } = company
 
     return {
       name,
       comment,
       logo,
       site,
+      companyName: company.name,
     }
   }
 
   @ApiModelProperty({ example: false, required: false })
   public readonly empty?: boolean
 
-  @ApiModelProperty({ example: 'Google' })
+  @ApiModelProperty({ example: 'Google quota' })
   public readonly name?: string
+
+  @ApiModelProperty({ example: 'Google' })
+  public readonly companyName?: string
 
   @ApiModelProperty({ example: 'Гуляем на деньги гугла' })
   public readonly comment?: string
