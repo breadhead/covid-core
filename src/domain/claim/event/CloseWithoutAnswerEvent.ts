32aa42cb545ca9673d6dd81efbb0807cff60592d
iwanttobealight <iwanttobealight@yandex.ru>
2019-02-20 12:05:53 +0300

    fix(claim): fix types

--- src/domain/claim/event/CloseWithoutAnswerEvent.ts
+++ src/domain/claim/event/CloseWithoutAnswerEvent.ts
@@ -2,8 +2,8 @@ import Claim from '@app/domain/claim/Claim.entity'
 import Event from '@app/infrastructure/events/Event'
 
 export const NAME = 'claim/close-without-answer'
-// TODO: типы
-export default class CloseWithoutAnswer implements Event<any> {
+
+export default class CloseWithoutAnswer implements Event<Claim> {
   public readonly name = NAME
 
   public constructor(public readonly payload: any) {}
