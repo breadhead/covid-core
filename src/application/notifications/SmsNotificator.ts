bb120c20f9de6131bbf74df8d2cde5cc9b69ed39
iwanttobealight <iwanttobealight@yandex.ru>
2019-03-12 18:32:43 +0300

    feat(sms notificator): fleshed out sms links

--- src/application/notifications/SmsNotificator.ts
+++ src/application/notifications/SmsNotificator.ts
@@ -16,7 +16,7 @@ import Notificator from './Notificator'
 
 export default class SmsNotificator implements Notificator {
   private readonly send: (to: string, content: string) => Promise<void>
-  private readonly defaultConext: object
+  private siteUrl: string
 
   public constructor(
     @Inject(SmsSenderSymbol) smsSender: SmsSender,
@@ -25,32 +25,22 @@ export default class SmsNotificator implements Notificator {
   ) {
     this.send = (to, content) => smsSender.send(to, content)
 
-    const link = config.get('SITE_URL').getOrElse('localhost')
-    let shortLink = null
-
-    axios
-      .get(`https://clck.ru/--?url=${link}`)
-      .then(response => {
-        shortLink = response.data
-      })
-      .catch(error => {
-        throw new SmsException(error, link)
-      })
-
-    this.defaultConext = {
-      link: shortLink || link,
-    }
+    this.siteUrl = config.get('SITE_URL').getOrElse('localhost')
   }
 
   public async newChatMessageFromSpecialist(message: Message): Promise<void> {
-    const { number, author } = message.claim
+    const { number, author, id } = message.claim
     const { name } = message.claim.applicant
+
+    const link = await this.getShortLink(
+      `${this.siteUrl}/client/consultation/${id}?openMessage`,
+    )
 
     if (author.contacts.phone) {
       const text = await this.templating.render(
         'sms/new-chat-message-from-specialist',
         {
-          ...this.defaultConext,
+          link: link || this.siteUrl,
           number,
           name,
         },
@@ -81,12 +71,16 @@ export default class SmsNotificator implements Notificator {
   }
 
   public async shortClaimQueued(claim: Claim): Promise<void> {
-    const { number, author } = claim
+    const { number, author, id } = claim
     const { name } = claim.applicant
+
+    const link = await this.getShortLink(
+      `${this.siteUrl}/client/consultation/${id}`,
+    )
 
     if (author.contacts.phone) {
       const text = await this.templating.render('sms/claim-queued', {
-        ...this.defaultConext,
+        link: link || this.siteUrl,
         number,
         name,
       })
@@ -99,9 +93,13 @@ export default class SmsNotificator implements Notificator {
     const { number, author } = claim
     const { name } = claim.applicant
 
+    const link = await this.getShortLink(
+      `${this.siteUrl}/contacts#feedback-form`,
+    )
+
     if (author.contacts.phone) {
       const text = await this.templating.render('sms/claim-rejected', {
-        ...this.defaultConext,
+        link: link || this.siteUrl,
         number,
         name,
       })
@@ -111,17 +109,32 @@ export default class SmsNotificator implements Notificator {
   }
 
   public async doctorAnswer(claim: Claim): Promise<void> {
-    const { number, author } = claim
+    const { number, author, id } = claim
     const { name } = claim.applicant
+
+    const link = await this.getShortLink(
+      `${this.siteUrl}/client/consultation/${id}#expert-answers`,
+    )
 
     if (author.contacts.email) {
       const text = await this.templating.render('sms/doctor-answer', {
-        ...this.defaultConext,
+        link: link || this.siteUrl,
         number,
         name,
       })
 
       await this.send(author.contacts.phone, text)
     }
+  }
+
+  private async getShortLink(link: string): Promise<any> {
+    return axios
+      .get(`https://clck.ru/--?url=${link}`)
+      .then(response => {
+        return response.data
+      })
+      .catch(error => {
+        throw new SmsException(error, link)
+      })
   }
 }
