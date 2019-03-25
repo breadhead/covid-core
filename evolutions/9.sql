e29f67bb90bec67afd5c84b4f37e7f5295a096ef
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2019-03-25 11:53:04 +0300

    Update evolutions/9.sql
    Co-Authored-By: igorkamyshev <garik.novel@gmail.com>

--- evolutions/9.sql
+++ evolutions/9.sql
@@ -2,7 +2,7 @@ ALTER TABLE claim
     ADD COLUMN _sentToDoctorAt DATETIME DEFAULT NULL,
     ADD COLUMN _sentToClientAt DATETIME DEFAULT NULL,
     ADD COLUMN _closedAt DATETIME DEFAULT NULL,
-    ADD COLUMN closedBy ENUM('client', 'case-manager', 'admin', 'docror') DEFAULT NULL;
+    ADD COLUMN closedBy ENUM('client', 'case-manager', 'admin', 'doctor') DEFAULT NULL;
 
 #DOWN
 
