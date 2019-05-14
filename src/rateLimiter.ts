e0770f9d671ef11528687f0a0fea5d904bd69a7a
Igor Kamyshev <igor@kamyshev.me>
2019-05-14 13:36:19 +0300

    Update rateLimiter.ts

--- src/rateLimiter.ts
+++ src/rateLimiter.ts
@@ -6,7 +6,7 @@ export const rateLimiter = (app: INestExpressApplication) => {
 
   const limiter = new RateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 1000, // limit each IP to 100 requests per windowMs
+    max: 1000, // limit each IP to 1000 requests per windowMs
   })
 
   return limiter
