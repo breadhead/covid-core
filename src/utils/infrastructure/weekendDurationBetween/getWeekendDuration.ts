24d70fef44a8b45c796b00a7b75b5376210ff9d3
iwanttobealight <iwanttobealight@yandex.ru>
2019-07-04 19:24:06 +0300

    refactor(get-weekend-duration): separate to several funcs

--- src/utils/infrastructure/weekendDurationBetween/getWeekendDuration.ts
+++ src/utils/infrastructure/weekendDurationBetween/getWeekendDuration.ts
@@ -1,64 +1,21 @@
-import {
-  isWeekend,
-  eachDay,
-  endOfDay,
-  differenceInMilliseconds,
-  addDays,
-  startOfDay,
-  subDays,
-} from 'date-fns'
+import { isWeekend } from 'date-fns'
 
 import LogicException from '@app/presentation/http/exception/LogicException'
-import { MS_IN_DAY } from './MS_IN_DAY'
+import { getIntervalWithoutHolidays } from './getIntervalWithoutHolidays'
+import { getIntervalStartedOnHoliday } from './getIntervalStartedOnHoliday'
+import { getIntervalEndedOnHoliday } from './getIntervalEndedOnHoliday'
 
 export const getWeekendDuration = (start: Date, end: Date) => {
   if (!isWeekend(start) && !isWeekend(end)) {
-    const days = eachDay(start, end)
-    const holidays = days.filter(isWeekend).length
-
-    return holidays * MS_IN_DAY
+    return getIntervalWithoutHolidays(start, end)
   }
 
   if (isWeekend(start)) {
-    const endOfTheCurrentDayCandidate = endOfDay(start)
-    // на случай если начало и конец находятся в одном дне
-    const endOfTheCurrentDay =
-      endOfTheCurrentDayCandidate > end ? end : endOfTheCurrentDayCandidate
-
-    const durationOfStartDay = Math.abs(
-      differenceInMilliseconds(start, endOfTheCurrentDay),
-    )
-    const nextDay = addDays(endOfTheCurrentDay, 1)
-    if (isWeekend(nextDay)) {
-      const full = nextDay < end
-
-      const nextDayDuration = full
-        ? MS_IN_DAY
-        : Math.abs(differenceInMilliseconds(endOfTheCurrentDay, end))
-
-      return nextDayDuration + durationOfStartDay
-    }
-
-    return durationOfStartDay
+    return getIntervalStartedOnHoliday(start, end)
   }
 
   if (isWeekend(end)) {
-    const startOfTheCurrentDay = startOfDay(end)
-    const durationOfEndDay = Math.abs(
-      differenceInMilliseconds(startOfTheCurrentDay, end),
-    )
-
-    const prevDay = subDays(startOfTheCurrentDay, 1)
-    if (isWeekend(prevDay)) {
-      const full = prevDay > start
-
-      const prevDayDuration = full
-        ? MS_IN_DAY
-        : Math.abs(differenceInMilliseconds(start, startOfTheCurrentDay))
-      return prevDayDuration + durationOfEndDay
-    }
-
-    return durationOfEndDay
+    return getIntervalEndedOnHoliday(start, end)
   }
 
   throw new LogicException('')
