51cd49c4f7d4624972649fc4061138c21754e75e
Igor Kamyshev <igor@kamyshev.me>
2019-07-16 10:04:19 +0300

    Update StatisticsController.ts

--- src/presentation/http/controller/StatisticsController.ts
+++ src/presentation/http/controller/StatisticsController.ts
@@ -78,7 +78,6 @@ export default class StatisticsController {
   @Get('success-closed-claims-count')
   @ApiOperation({ title: 'number of successfully closed claims' })
   @ApiOkResponse({ description: 'Success' })
-  @ApiForbiddenResponse({ description: 'Admin API token doesn`t provided' })
   public async generateSuccessClaimsCount(): Promise<number> {
     const closedClaimsCount = await this.claimRepo.getSuccessClaimsCount()
     return closedClaimsCount
