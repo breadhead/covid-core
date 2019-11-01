cff11594dd6f41e20246c745a1f6ca261aa23936
iwanttobealight <iwanttobealight@yandex.ru>
2019-11-01 16:01:35 +0300

    feat(20 evolution): update questions

--- evolutions/20.sql
+++ evolutions/20.sql
@@ -1,14 +1,12 @@
 INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q1', 'value', 'Оцените, пожалуйста, насколько быстро вы получили ответ эксперта?', 'Подсказка к вопросу: 1 - очень долго, 10 - очень быстро');
 
-INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q2', 'value', 'Насколько понятен был ответ?', 'Подсказка к вопросу: 1 - абсолютно непонятно, 10 - полностью понятно');
+INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q2', 'value', 'Насколько полным был ответ?', 'Подсказка к вопросу: 1 - неполный, 10 - полный');
 
-INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q3', 'value', 'Насколько полным был ответ?', 'Подсказка к вопросу: 1 - неполный, 10 - полный');
+INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q3', 'value', 'Насколько понятен был ответ?', 'Подсказка к вопросу: 1 - совсем непонятно, 10 - полностью понятно');
 
-INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q4', 'value', 'Насколько удобно было пользоваться сервисом (заполнять анкету, читать ответ, прикреплять файлы)?', 'Подсказка к вопросу: 1 - очень неудобно, 10 - очень удобно');
+INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q4', 'value', 'Насколько полезным был ответ?', 'Подсказка к вопросу: 1 - абсолютно бесполезен, 10 - очень полезен');
 
-INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q5', 'value', 'Насколько полезным был ответ?', 'Подсказка к вопросу: 1 - абсолютно бесполезен, 10 - очень полезен');
-
-INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q6', 'comment', 'Что вам особенно понравилось/не понравилось?', 'Подсказка к вопросу: Вы очень нам поможете, если поделитесь своим опытом');
+INSERT INTO ratingQuestions(id, _type, _question, _hint) VALUES('Q5', 'comment', 'Что вам особенно понравилось/не понравилось?', 'Подсказка к вопросу: Вы очень нам поможете, если поделитесь своим опытом');
 
 #DOWN
 DELETE FROM ratingQuestions WHERE id='Q1';
