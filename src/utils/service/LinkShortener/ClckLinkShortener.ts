338034d4863cbfab1b190d52e253612dae143a12
iwanttobealight <iwanttobealight@yandex.ru>
2019-11-01 14:16:47 +0300

    feat(link shorter): add logger

--- src/utils/service/LinkShortener/ClckLinkShortener.ts
+++ src/utils/service/LinkShortener/ClckLinkShortener.ts
@@ -2,10 +2,15 @@ import { Injectable } from '@nestjs/common'
 import axios from 'axios'
 
 import { LinkShortener } from './LinkShortener'
+import { Logger } from '../Logger/Logger'
 
 @Injectable()
 export class ClckLinkShortener implements LinkShortener {
+  public constructor(private readonly logger: Logger) {}
+
   getShort(link: string) {
+    this.logger.warn(link)
+
     return axios
       .get(`'https://clck.ru/--?url='${link}`)
       .then(response => response.data)
