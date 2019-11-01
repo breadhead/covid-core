1e751612c2a867ef4255efdbba8f99bb96d6fe98
iwanttobealight <iwanttobealight@yandex.ru>
2019-11-01 14:19:46 +0300

    feat(link shorter): add logger in catch

--- src/utils/service/LinkShortener/ClckLinkShortener.ts
+++ src/utils/service/LinkShortener/ClckLinkShortener.ts
@@ -6,7 +6,7 @@ import { Logger } from '../Logger/Logger'
 
 @Injectable()
 export class ClckLinkShortener implements LinkShortener {
-  public constructor(private readonly logger: Logger) {}
+  public constructor(private readonly logger: Logger) { }
 
   getShort(link: string) {
     this.logger.warn(link)
@@ -14,6 +14,10 @@ export class ClckLinkShortener implements LinkShortener {
     return axios
       .get(`'https://clck.ru/--?url='${link}`)
       .then(response => response.data)
-      .catch(() => link)
+      .catch(() => {
+        this.logger.warn(`error ${link}`)
+        return link
+      }
+      )
   }
 }
