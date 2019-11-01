74045cfcd285b7e078e696e116f54e6627c435b1
iwanttobealight <iwanttobealight@yandex.ru>
2019-11-01 15:04:32 +0300

    feat(sms-motificator): add console.log

--- src/utils/service/LinkShortener/ClckLinkShortener.ts
+++ src/utils/service/LinkShortener/ClckLinkShortener.ts
@@ -6,7 +6,7 @@ import { Logger } from '../Logger/Logger'
 
 @Injectable()
 export class ClckLinkShortener implements LinkShortener {
-  public constructor(private readonly logger: Logger) { }
+  public constructor(private readonly logger: Logger) {}
 
   getShort(link: string) {
     this.logger.warn(link)
@@ -15,7 +15,7 @@ export class ClckLinkShortener implements LinkShortener {
     return axios
       .get(`'https://clck.ru/--?url='${link}`)
       .then(response => response.data)
-      .catch((error) => {
+      .catch(error => {
         this.logger.warn(`error ${link}`)
         console.log(`error ${link}`)
         console.log('error here', error)
