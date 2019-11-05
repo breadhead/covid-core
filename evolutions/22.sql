88294a1e25c6c624dac9896463c8004e429d1f79
iwanttobealight <iwanttobealight@yandex.ru>
2019-11-05 16:52:30 +0300

    feat(story): add controller

--- evolutions/22.sql
+++ evolutions/22.sql
@@ -1,9 +1,9 @@
 CREATE TABLE story
 (
     id VARCHAR(255) PRIMARY KEY NOT NULL,
+    `createdAt` DATETIME DEFAULT NULL,
     _claimId VARCHAR(255),
     CONSTRAINT FK_STORY_CLAIM FOREIGN KEY (_claimId) REFERENCES claim (id),
-    `createdAt` DATETIME DEFAULT NULL,
     phone VARCHAR(255),
     status ENUM('Не звонили', 'Звонили') DEFAULT 'Не звонили' NOT NULL
 );
---
+++ src/presentation/http/controller/StoryController.ts
@@ -0,0 +1,49 @@
+import { Controller, UseGuards, Post, Body, Get } from '@nestjs/common'
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUseTags,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+} from '@nestjs/swagger'
+import { InjectRepository } from '@nestjs/typeorm'
+
+import JwtAuthGuard from '../security/JwtAuthGuard'
+import RatingRepository from '@app/domain/rating/RatingRepository'
+import RatingQuestionsRepository from '@app/domain/rating-questions/RatingQuestionsRepository'
+import { EntityManager } from 'typeorm'
+import { IdGenerator } from '@app/utils/service/IdGenerator/IdGenerator'
+import StoryAddPhoneRequest from '../request/StoryAddPhoneRequest'
+
+@Controller('story')
+@UseGuards(JwtAuthGuard)
+@ApiUseTags('story')
+@ApiBearerAuth()
+export default class StoryController {
+  public constructor(
+    @InjectRepository(RatingRepository)
+    private readonly ratingRepo: any,
+    @InjectRepository(RatingQuestionsRepository)
+    private readonly ratingQuestionsRepo: any,
+    private readonly em: EntityManager,
+    private readonly idGenerator: IdGenerator,
+  ) {}
+
+  @Post('add-phone')
+  @ApiOperation({ title: 'Add new phone' })
+  @ApiOkResponse({ description: 'Success' })
+  @ApiCreatedResponse({ description: 'Phone added' })
+  public async addPhone(@Body() request: StoryAddPhoneRequest): Promise<any> {
+    // const { claimId, phone, status } = request
+    // const id = this.idGenerator.get()
+    // const curStory = new Story(
+    //   id,
+    //   new Date(),
+    //   claimId,
+    //   phone,
+    //   status
+    // )
+    // await this.em.save(curStory)
+  }
+}
