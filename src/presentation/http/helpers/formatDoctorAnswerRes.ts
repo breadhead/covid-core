8ca09dcf0787c0c7b755c3212add03576599f32f
iwanttobealight <iwanttobealight@yandex.ru>
2020-01-29 16:27:44 +0300

    feat(doctor answers): refactoring

--- src/presentation/http/helpers/formatDoctorAnswerRes.ts
+++ src/presentation/http/helpers/formatDoctorAnswerRes.ts
@@ -5,36 +5,32 @@ export const formatDoctorAnswerRes = (
   doctors: DoctorStat[],
   rating: DoctorRatingResponse[],
 ): any => {
-  const res = doctors
-    .map(doctor => {
-      if (rating.length === 0) {
-        return doctor
-      }
+  const res = doctors.map(doctor => {
+    if (rating.length === 0) {
+      return doctor
+    }
 
-      const doc = rating.map(rat => {
-        if (doctor.name === rat.doctor) {
-          return {
-            ...doctor,
-            ratingAverage: rat.ratingAverage,
-            ratingMedian: rat.ratingMedian,
-          }
+    const doc = rating
+      .filter(rat => doctor.name === rat.doctor)
+      .map(rat => {
+        return {
+          ...doctor,
+          ratingAverage: rat.ratingAverage,
+          ratingMedian: rat.ratingMedian,
         }
-
-        return null
       })
 
-      const filteredDoc =
-        doc.filter(it => !!it).length > 0 ? doc.filter(it => !!it)[0] : null
+    const filteredDoc =
+      doc.filter(it => !!it).length > 0 ? doc.filter(it => !!it)[0] : null
 
-      return !!filteredDoc
-        ? filteredDoc
-        : {
-            ...doctor,
-            ratingAverage: 0,
-            ratingMedian: 0,
-          }
-    })
-    .filter(it => !!it)
+    return !!filteredDoc
+      ? filteredDoc
+      : {
+          ...doctor,
+          ratingAverage: 0,
+          ratingMedian: 0,
+        }
+  })
 
   return res
 }
